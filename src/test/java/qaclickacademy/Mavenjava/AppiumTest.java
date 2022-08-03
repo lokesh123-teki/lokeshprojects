f03e18fcb0df5aa6cf1021bcb13f7124d552319f
lokesh <lokeshteki88@gmail.com>
2022-08-03 12:46:55 +0530

    second commit

--- src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -7,11 +7,15 @@ public class AppiumTest {
 	public void Androidchecking()
 	{
 		System.out.println("hi");
+		System.out.println("hlsjdlsdo");
+		System.out.println("hajkdjsbuwebc");
 	}
 
 	@Test
 	public void IOSchecking2()
 	{
 		System.out.println("hello");
+		System.out.println("sfjweifninwcjdw");
+		System.out.println("hoinceiwnfini");
 	}
 }
